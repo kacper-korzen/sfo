cbe5f94e988997a56b19c6df5436e5f074ab561a
Kacper <[k.kacper.korzeniewski@gmail.com]>
2026-04-25 00:38:48 +0200

    refactor: simplify create_backup function and typer command logic

--- src/sfo/backup.py
+++ src/sfo/backup.py
@@ -3,13 +3,14 @@ from datetime import datetime
 from pathlib import Path
 from typing import Annotated
 
+import rich
 import typer
 from rich.progress import Progress, SpinnerColumn, TextColumn
 
 from .config import BACKUP_NAME
 
 
-def create_backup(destination: str = ".") -> None:
+def create_backup() -> None:
     """
     Creates a zip archive of the contents of the current working directory.
 
@@ -19,19 +20,13 @@ def create_backup(destination: str = ".") -> None:
                      parent directory and then moved to the current directory
                      to mimic the original behavior.
     """
-
     timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M")
+    archive_name = f"{BACKUP_NAME}{timestamp}"
 
     current_path = Path.cwd()
     parent_path = current_path.parent
-
-    archive_name = f"{BACKUP_NAME}{timestamp}"
-
-    if destination == ".":
-        shutil.make_archive(f"{parent_path}/{archive_name}", "zip", current_path)
-        shutil.move(f"{parent_path}/{archive_name}.zip", current_path)
-    else:
-        shutil.make_archive(f"{destination}/{archive_name}", "zip", current_path)
+    shutil.make_archive(f"../backup/{archive_name}", "zip")
+    shutil.move(f"{parent_path}/backup/", current_path)
 
 
 # cli
@@ -39,16 +34,10 @@ app = typer.Typer()
 
 
 @app.command()
-def backup(
-    dest: Annotated[
-        str,
-        typer.Option("--path", "-p", help="Path to where to save archive"),
-    ] = ".",
-) -> None:
+def backup() -> None:
     """
-    Performs a directory backup into a zip archive.
-    The destination path specifies where the archive should be saved.
+    Performs a directory backup into a zip archive in current directory.
     """
     with Progress(SpinnerColumn(), TextColumn("Creating backup...")) as progress:
         task = progress.add_task("Zipping files", total=None)
-        create_backup(destination=dest)
+        create_backup()
