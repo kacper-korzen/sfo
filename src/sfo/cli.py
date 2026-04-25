c0395bf2f8c34428d2dea4e918518433f13bfa23
Kacper <[k.kacper.korzeniewski@gmail.com]>
2026-04-25 03:20:02 +0200

    feat(cli): import newly created commands into CLI entrypoint

--- src/sfo/cli.py
+++ src/sfo/cli.py
@@ -1,8 +1,15 @@
-from pathlib import Path
-from typing import Annotated
-
 import typer
 
-app = typer.Typer(name="Smart File Organizer")
+from .backup import app as backup_app
+from .organizer import app as organizer_app
+from .restore import app as restore_app
+
+app = typer.Typer()
+
+app.add_typer(organizer_app)
+app.add_typer(backup_app)
+app.add_typer(restore_app)
 
 
+if __name__ == "__main__":
+    app()
